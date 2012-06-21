f91fcff2c1842c28cab3709971a3456a76e957bd
AITI Student <aiti-student@aiti-093-PC.(none)>
2012-06-21 13:09:07 -0400

    solutions

---
+++ cipher.py
@@ -0,0 +1,5 @@
+encoded_phrase=''
+encodes_phrase=raw_input("enter a phrase to be coded:")
+shift_value=input("enter number of shifts:")
+for c in encoded_phrase:
+    encoded_phrase= encoded_phrase+c
