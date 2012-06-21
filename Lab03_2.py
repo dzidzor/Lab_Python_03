8f653ce89f058721a69041d213b3bc0c70605585
AITI Student <aiti-student@aiti-093-PC.(none)>
2012-06-21 13:07:43 -0400

    partial solutions

---
+++ Lab03_2.py
@@ -0,0 +1,31 @@
+
+
+
+#Lab03_2a
+for n in range (0,7):
+    for m in range(1,n,1):
+        print m,
+    print""
+
+    
+print""    
+
+#Lab03_2b
+for n in range (7,0,-1):
+    for m in range(1,n,1):
+        print m,
+    n=n-1
+    print""
+
+#Lab03_2c
+for n in range (0,7):
+    for m in range (1,n,1):
+        print m,
+    n=n+1
+    print""
+    
+    
+    
+
+    
+
