95083ce83226cbf7c06d133e0abb3b6eb535ce97
AITI Student <aiti-student@aiti-093-PC.(none)>
2012-06-22 07:05:13 -0400

    ....

--- Lab03_2.py
+++ Lab03_2.py
@@ -3,7 +3,7 @@
 
 #Lab03_2a
 for n in range (0,7):
-    for m in range(1,n,1):
+    for m in range(1,n+1,1):
         print m,
     print""
 
@@ -14,18 +14,34 @@ print""
 for n in range (7,0,-1):
     for m in range(1,n,1):
         print m,
-    n=n-1
     print""
+print""
 
 #Lab03_2c
-for n in range (0,7):
-    for m in range (1,n,1):
-        print m,
-    n=n+1
+for n in range (1,7,1):
+    for m in range (7-n,1,-1):
+        print "",
+    for p in range(n,0,-1):
+        print p,
     print""
     
     
-    
+#Lab03_2d
+for n in range (7,0,-1):
+    for m in range (7-n,0,-1):
+        print "",
+    for p in range(1,n,1):
+        print p,
+    print""
+        
 
-    
-
+#Lab03_2e
+for n in range (1,7,1):
+    for m in range (7-n,1,-1):
+        print "",
+    for p in range(n-1,0,-1):
+        print p,
+    for q in range(2,n,1):
+        print q,
+    print""
+        
